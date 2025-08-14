34643292d5656093c8f59177d82f5d79d058af83
ccastagnola <catherinecastagnola@gmail.com>
2025-08-13 22:09:43 -0400

    Finished First Prototype, updated data handling

--- src/app/appData/posts.ts
+++ src/app/appData/posts.ts
@@ -4,14 +4,9 @@
 //In future version, data will be on MongoDB cluster and not local
 
 import Post
- from "../Types";
+    from "../Types";
 
-let posts: Post[] = [
-    { id: "1", title: "Flowers", content: "My partner got me an orchid plant!", image: "https://upload.wikimedia.org/wikipedia/commons/4/45/Phalaenopsis_c.v._orchid_%2849367895198%29.jpg" },
-    { id: "2", title: "Angry Birds", content: "A classic game from 2016." },
-    { id: "3", title: "Yummy Coffee", content: "I had a nice iced coffee today." },
-    { id: "4", title: "John Wick", content: "Just rewatched the first movie, it's from 2014!" },
-];
+let posts: Post[] = [];
 
 //getter method for the array list of all posts
 export const getPosts = () => {
@@ -19,8 +14,8 @@ export const getPosts = () => {
 };
 
 //add a new post
-export const addNewPost = (postData: {title: string, content: string, image?:string}) => {
-    const newPost: Post ={
+export const addNewPost = (postData: { title: string, content: string, image?: string }) => {
+    const newPost: Post = {
         id: posts.length.toString(),
         ...postData,
     };
--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -1,28 +1,42 @@
-"use client"
+"use client";
 
 import { Box, TextField, Divider, Button } from "@mui/material";
 import MenuBar from "../Components/menubar";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { addNewPost } from "../appData/posts";
 
 export default function Home() {
+    const[title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const router = useRouter();
 
     const handlePostClick = () => {
-        setContent("")
+        if(title.trim() && content.trim()){
+            addNewPost({title, content});
+            router.push("/");
+        }
     }
 
     return (
         <Box margin={1}>
             <MenuBar title={"Hygge"}></MenuBar>
             <Box textAlign={"center"} alignItems={"center"}>
+                <TextField 
+                    variant="filled"
+                    fullWidth
+                    size="small"
+                    label="Enter your Title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    />
                 <TextField
-                    id="filled-basic"
                     variant="filled"
                     margin="dense"
                     multiline
                     fullWidth
                     minRows={3}
-                    sx={{}}
+                    label="Enter your post description."
+                    onChange={(e) => setContent(e.target.value)}
                 />
                 <Divider />
                 <Box>
